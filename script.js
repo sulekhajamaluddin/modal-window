8417216334052d510bfc26c62b3ededa73f46e1f
Sulekha Jamaluddin <sulu.lekha@gmail.com>
2021-12-30 14:58:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+const buttonsShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const buttonCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < buttonsShowModal.length; i++)
+  buttonsShowModal[i].addEventListener('click', openModal);
+
+buttonCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
